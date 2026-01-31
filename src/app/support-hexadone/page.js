7151bacac7eb73ba857c718f1d4992c7297ee60c
hedi larbi <hedilarbi@hedis-MacBook-Air.local>
2026-01-31 11:54:50 +0100

    suppoer

---
+++ src/app/support-hexadone/page.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="w-full h-full relative overflow-hidden pb-20 mt-32">
+      <h1 className="font-inter font-semibold lg:text-6xl text-2xl text-white text-center">
+        Support Hexadone Page
+      </h1>
+      <p className="text-white">
+        This application is intended for internal use by company members. For
+        support requests: Email: support@hexadone.fr
+      </p>
+    </div>
+  );
+};
+
+export default page;
