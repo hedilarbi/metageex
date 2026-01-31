6ecb7d653a4de97f3825ffcbf6ee35d9197e73c9
hedi larbi <hedilarbi@hedis-MacBook-Air.local>
2026-01-31 12:03:30 +0100

    suppoer

--- src/app/support-hexadone/page.js
+++ src/app/support-hexadone/page.js
@@ -2,14 +2,16 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="w-full h-full relative overflow-hidden pb-20 mt-32">
-      <h1 className="font-inter font-semibold lg:text-6xl text-2xl text-white text-center">
-        Support Hexadone Page
-      </h1>
-      <p className="text-white">
-        This application is intended for internal use by company members. For
-        support requests: Email: support@hexadone.fr
-      </p>
+    <div className="w-full h-full relative overflow-hidden pb-20 ">
+      <div className="mt-32 h-screen">
+        <h1 className="font-inter font-semibold lg:text-6xl text-2xl text-white text-center">
+          Support Hexadone Page
+        </h1>
+        <p className="text-white mt-12 px-12">
+          The application Départements Data is intended for internal use by
+          company members. For support requests: Email: support@hexadone.fr
+        </p>
+      </div>
     </div>
   );
 };
